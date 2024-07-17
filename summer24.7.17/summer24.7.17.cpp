279b3807d7c17f67bebef4d20a0c43f7911e0f25
lyx <doo8opnewman@outlook.com>
2024-07-17 22:29:12 +0800

    添加项目文件。

---
+++ summer24.7.17/summer24.7.17.cpp
@@ -0,0 +1,24 @@
+﻿//高精度乘法
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+string multiply(string a, int b)
+{
+	string c;
+	int len = a.length();
+	int carry = 0;//进位
+	for (int i = 0; i < len || carry; i++)//有进位也要继续
+	{
+		if (i < len)carry += ((int)a[len-i-1]-48) * b;
+		c += (carry % 10)+'0';
+		carry /= 10;
+	}
+	reverse(c.begin(), c.end());
+	return c;
+}
+int main()
+{
+	cout<<multiply("987", 2);
+}
